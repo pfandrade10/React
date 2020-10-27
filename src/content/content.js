ff521550ddb01bc87dacd6b1cfe2aad385143c41
Pedro Andrade <pfandrade10@hotmail.com>
2020-10-26 22:37:03 -0300

    correções

---
+++ src/content/content.js
@@ -0,0 +1,16 @@
+import './navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+
+function MainContent() {
+    return (
+        <>
+        <div>
+
+        </div>
+        </>
+        );
+    }
+    
+    export default MainContent;
