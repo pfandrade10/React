4b2c0a6949820bcca0e34c8cd42739298e28ef05
Pedro Andrade <pfandrade10@hotmail.com>
2020-11-08 21:26:38 -0300

    correções

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+
+var contador = 4;
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="row">
+        <div className="col-md-5">
+          <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices congue imperdiet. Mauris venenatis augue nec iaculis egestas. Praesent at dolor massa. Nulla ac augue leo. Integer id dignissim risus. Morbi pretium elit nibh. Mauris lectus nulla, aliquam at eros ac, dictum bibendum eros. Fusce sed commodo nisl. Fusce finibus massa sem, at ullamcorper tellus finibus vitae. Sed porttitor turpis tortor, a condimentum dui scelerisque quis.
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card-img">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_hecBTV6iuRGd74oxos71_FaYxzD60Nk0VQ&usqp=CAU"></img>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-5">
+          <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices congue imperdiet. Mauris venenatis augue nec iaculis egestas. Praesent at dolor massa. Nulla ac augue leo. Integer id dignissim risus. Morbi pretium elit nibh. Mauris lectus nulla, aliquam at eros ac, dictum bibendum eros. Fusce sed commodo nisl. Fusce finibus massa sem, at ullamcorper tellus finibus vitae. Sed porttitor turpis tortor, a condimentum dui scelerisque quis.
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card-img">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_hecBTV6iuRGd74oxos71_FaYxzD60Nk0VQ&usqp=CAU"></img>
+          </div>
+        </div>
+      </div>
+     
     </div>
+    </>
   );
 }
 
